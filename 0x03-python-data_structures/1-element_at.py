ef103b9c7ba0e12361d1607f7445e5f58470721f
Mizan-Assefa <99024318+Mizan-Assefa@users.noreply.github.com>
2022-07-05 22:52:25 +0300

    task

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,6 +3,6 @@ def element_at(my_list, idx):
     if idx < 0:
         return(None)
     elif len(my_list) <= idx:
-            return(None)
-        else:
-            return(my_list[idx])
+        return(None)
+    else:
+        return(my_list[idx])
