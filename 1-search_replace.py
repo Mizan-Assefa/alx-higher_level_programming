6cfa85c44f67cf2f74f5211e199c5836d5213961
Mizan-Assefa <99024318+Mizan-Assefa@users.noreply.github.com>
2022-07-06 20:20:52 +0300

    Delete 1-search_replace.py

--- 1-search_replace.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def search_replace(my_list, search, replace):
-    if len(my_list) != 0:
-        new_list = my_list[:]
-        for x in new_list:
-            r = map(lambda x: replace if x == search else x, new_list)
-        return(list(r))
-    else:
-        return(my_list)
